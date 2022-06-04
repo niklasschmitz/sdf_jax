6e78b3f88b648c9dbfb08ac31f0d611dfc8bb715
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2022-06-05 00:26:43 +0200

    use contourf for sdf plot2d

--- sdf_jax/util.py
+++ sdf_jax/util.py
@@ -1,12 +1,13 @@
 from sdf_jax.discretize import discretize2d, discretize3d
 
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from skimage import measure
 
 
-def plot2d(sdf, xy_lims=(0,1), ngrid=10):
+def plot2d(sdf, xy_lims=(0, 1), ngrid=10, scatter_pts=None):
     xs, ys = discretize2d(sdf, xy_lims, ngrid)
     fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(6, 3))
     ax0.set_title("zero levelset")
@@ -14,15 +15,18 @@ def plot2d(sdf, xy_lims=(0,1), ngrid=10):
     ax0.set_xticks(xy_lims)
     ax0.set_yticks(xy_lims)
     ax1.set_title("SDF")
-    c = ax1.imshow(ys, origin='lower')
-    ax1.contour(ys, levels=[0.], colors=["salmon"])
-    ax1.set_xticks([0,ngrid-1])
-    ax1.set_xticklabels(xy_lims)
-    ax1.set_yticks([0,ngrid-1])
-    ax1.set_yticklabels(xy_lims)
+    # c = ax1.imshow(ys, origin='lower')
+    c = ax1.contourf(xs[:,:,0], xs[:,:,1], ys, levels=np.arange(-1,1,0.1))
+    ax1.contour(xs[:,:,0], xs[:,:,1], ys, levels=[0.], colors=["w"])
+    if scatter_pts is not None:
+        ax0.scatter(*scatter_pts.T)
+        ax1.scatter(*scatter_pts.T, c="w", alpha=0.5)
+    ax1.set_xticks(xy_lims)
+    ax1.set_yticks(xy_lims)
     divider = make_axes_locatable(plt.gca())
     cax = divider.append_axes("right", "5%", pad="3%")
-    plt.colorbar(c, cax=cax)
+    cb = plt.colorbar(c, cax=cax)
+    cb.ax.plot([0, 1], [0.0]*2, c="w")
     plt.tight_layout()
     plt.close()
     return fig
@@ -41,6 +45,8 @@ def plot3d(sdf, xyz_lims=(0, 1), ngrid=10):
     ax.set_ylim(*xyz_lims)
     ax.set_zlim(*xyz_lims)
     plt.tight_layout()
+    plt.close()
+    return fig
 
 def dataloader(xs, ys, batch_size, *, key):
     dataset_size = xs.shape[0]
