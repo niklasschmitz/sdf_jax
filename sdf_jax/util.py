f85683576beb8db57104ff14b89f562eada446eb
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2022-06-14 10:30:07 +0200

    Fix imports

--- sdf_jax/util.py
+++ sdf_jax/util.py
@@ -1,5 +1,7 @@
 from sdf_jax.discretize import discretize2d, discretize3d
 
+import jax.numpy as jnp
+import jax.random as jrandom
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
