9f00c7655008b70c5c5b890bc29bd95d5e6957d7
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2022-06-24 16:49:09 +0200

    Refactor discretization bounds

--- sdf_jax/discretize.py
+++ sdf_jax/discretize.py
@@ -4,19 +4,19 @@ from jax import vmap
 import functools as ft
 
 
-@ft.partial(jax.jit, static_argnums=(0,2))
-def discretize2d(sdf, xy_lims, ngrid):
-    xs = jnp.meshgrid(jnp.linspace(*xy_lims, ngrid), 
-                      jnp.linspace(*xy_lims, ngrid))
+@ft.partial(jax.jit, static_argnums=(0,1))
+def discretize2d(sdf, ngrid, x_lims, y_lims):
+    xs = jnp.meshgrid(jnp.linspace(*x_lims, ngrid), 
+                      jnp.linspace(*y_lims, ngrid))
     xs = jnp.stack(xs, axis=-1) # (nx, ny, 2)
     ys = vmap(vmap(sdf))(xs)
     return xs, ys
 
-@ft.partial(jax.jit, static_argnums=(0,2))
-def discretize3d(sdf, xyz_lims, ngrid):
-    xs = jnp.meshgrid(jnp.linspace(*xyz_lims, ngrid),
-                      jnp.linspace(*xyz_lims, ngrid),
-                      jnp.linspace(*xyz_lims, ngrid))
+@ft.partial(jax.jit, static_argnums=(0,1))
+def discretize3d(sdf, ngrid, x_lims, y_lims, z_lims):
+    xs = jnp.meshgrid(jnp.linspace(*x_lims, ngrid),
+                      jnp.linspace(*y_lims, ngrid),
+                      jnp.linspace(*z_lims, ngrid))
     xs = jnp.stack(xs, axis=-1) # (nx, ny, nz, 3)
     ys = vmap(vmap(vmap(sdf)))(xs)
     return xs, ys
--- sdf_jax/util.py
+++ sdf_jax/util.py
@@ -9,22 +9,23 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from skimage import measure
 
 
-def plot2d(sdf, xy_lims=(0, 1), ngrid=10, scatter_pts=None):
-    xs, ys = discretize2d(sdf, xy_lims, ngrid)
+def plot2d(sdf, *, ngrid=10, x_lims=(0, 1), y_lims=None, scatter_pts=None):
+    if y_lims is None:
+        y_lims = x_lims
+    xs, ys = discretize2d(sdf, ngrid, x_lims, y_lims)
     fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(6, 3))
     ax0.set_title("zero levelset")
     ax0.contour(xs[:,:,0], xs[:,:,1], ys, levels=[0.], colors=["salmon"])
-    ax0.set_xticks(xy_lims)
-    ax0.set_yticks(xy_lims)
+    ax0.set_xticks(x_lims)
+    ax0.set_yticks(y_lims)
     ax1.set_title("SDF")
-    # c = ax1.imshow(ys, origin='lower')
     c = ax1.contourf(xs[:,:,0], xs[:,:,1], ys, levels=np.arange(-1,1,0.1))
     ax1.contour(xs[:,:,0], xs[:,:,1], ys, levels=[0.], colors=["w"])
     if scatter_pts is not None:
         ax0.scatter(*scatter_pts.T)
         ax1.scatter(*scatter_pts.T, c="w", alpha=0.5)
-    ax1.set_xticks(xy_lims)
-    ax1.set_yticks(xy_lims)
+    ax1.set_xticks(x_lims)
+    ax1.set_yticks(y_lims)
     divider = make_axes_locatable(plt.gca())
     cax = divider.append_axes("right", "5%", pad="3%")
     cb = plt.colorbar(c, cax=cax)
@@ -33,10 +34,17 @@ def plot2d(sdf, xy_lims=(0, 1), ngrid=10, scatter_pts=None):
     plt.close()
     return fig
 
-def plot3d(sdf, xyz_lims=(0, 1), ngrid=10):
-    xs, ys = discretize3d(sdf, xyz_lims, ngrid)
+def plot3d(sdf, *, ngrid=10, x_lims=(0, 1), y_lims=None, z_lims=None):
+    if y_lims is None:
+        y_lims = x_lims
+    if z_lims is None:
+        z_lims = x_lims
+    xs, ys = discretize3d(sdf, ngrid, x_lims, y_lims, z_lims)
     verts, faces, normals, values = measure.marching_cubes(np.array(ys), 0)
-    verts = (verts / ngrid) * abs(xyz_lims[0] - xyz_lims[1]) + xyz_lims[0]
+    verts /= ngrid
+    verts[:,0] = verts[:,0] * abs(x_lims[0] - x_lims[1]) + x_lims[0]
+    verts[:,1] = verts[:,1] * abs(y_lims[0] - y_lims[1]) + y_lims[0]
+    verts[:,2] = verts[:,2] * abs(z_lims[0] - z_lims[1]) + z_lims[0]
     return trimesh.Trimesh(vertices=verts, faces=faces, process=False).show()
 
 def dataloader(xs, ys, batch_size, *, key):
