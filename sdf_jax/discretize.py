eb563ee1e0e3cf8c6f7b1c98ea08d0dcfd88bf7b
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2022-09-13 13:22:15 +0200

    Use lax.map for outer loops for memory efficiency

--- sdf_jax/discretize.py
+++ sdf_jax/discretize.py
@@ -1,15 +1,19 @@
 import jax
 import jax.numpy as jnp
 from jax import vmap
+from jax import lax
 import functools as ft
 
+
+def lmap(f):
+    return lambda x: lax.map(f, x)
 
 @ft.partial(jax.jit, static_argnums=(0,1))
 def discretize2d(sdf, ngrid, x_lims, y_lims):
     xs = jnp.meshgrid(jnp.linspace(*x_lims, ngrid), 
                       jnp.linspace(*y_lims, ngrid))
     xs = jnp.stack(xs, axis=-1) # (nx, ny, 2)
-    ys = vmap(vmap(sdf))(xs)
+    ys = lmap(vmap(sdf))(xs)
     return xs, ys
 
 @ft.partial(jax.jit, static_argnums=(0,1))
@@ -18,5 +22,5 @@ def discretize3d(sdf, ngrid, x_lims, y_lims, z_lims):
                       jnp.linspace(*y_lims, ngrid),
                       jnp.linspace(*z_lims, ngrid))
     xs = jnp.stack(xs, axis=-1) # (nx, ny, nz, 3)
-    ys = vmap(vmap(vmap(sdf)))(xs)
+    ys = lmap(lmap(vmap(sdf)))(xs)
     return xs, ys
